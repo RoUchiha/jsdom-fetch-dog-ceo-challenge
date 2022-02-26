1b8f4c68c4b87ea7d915a14132b61d11a03bef8a
RoUchiha <swaggsikh@gmail.com>
2022-02-26 00:40:57 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,73 @@
 console.log('%c HI', 'color: firebrick')
+
+let dogBreeds = [];
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadPics();
+    loadBreeds();
+});
+
+function loadPics() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(pics => {
+        pics.message.forEach(pic => addPic(pic))
+    });
+}
+
+function addPic(dogPicUrl) {
+    let container = document.querySelector('#dog-image-container');
+    let newPicEle = document.createElement('img');
+    newPicEle.src = dogPicUrl;
+    container.appendChild(newPicEle);
+}
+
+function loadBreeds() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(response => response.json())
+    .then(resp => {
+        breeds = Object.keys(resp.message);
+        updateBreedList(breeds);
+        addBreedSelectListener();
+    });
+}
+
+function updateBreedList(breeds) {
+    let ul = document.querySelector('#dog-breeds');
+    removeChildren(ul);
+    breeds.forEach(breed => addBreed(breed));
+}
+
+function removeChildren(ele) {
+    let child = ele.lastElementChild;
+    while (child) {
+        ele.removeChild(child);
+        child = ele.lastElementChild;
+    }
+}
+
+function selectBreedsStartingWith(letter) {
+    updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+}
+
+function addBreedSelectListener() {
+    let breedDrop = document.querySelector('#breed-dropdown');
+    breedDrop.addEventListener('change', function (event) {
+        selectBreedsStartingWith(event.target.value);
+    });
+}
+
+function addBreed(breed) {
+    let ul = document.querySelector('#dog-breeds');
+    let li = document.createElement('li');
+    li.innerHTML = breed;
+    li.style.cursor = 'crosshair';
+    ul.appendChild(li);
+    li.addEventListener('click', changeColor);
+}
+
+function changeColor(event) {
+    event.target.style.color = 'pink';
+}
